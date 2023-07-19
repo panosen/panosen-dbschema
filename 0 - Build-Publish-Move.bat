aa6d3883f02befa750a05071fe78971f5b507e28
hc_zhang <hc_zhang@trip.com>
2023-07-19 10:18:38 +0800

    update bat

--- 0 - Build-Publish-Move.bat
+++ 0 - Build-Publish-Move.bat
@@ -4,9 +4,6 @@ dotnet restore
 
 dotnet build --no-restore -c Release
 
-dotnet nuget push Panosen.DBSchema.Mysql\bin\Release\Panosen.DBSchema.Mysql.*.nupkg -s https://package.savory.cn/v3/index.json --skip-duplicate
-dotnet nuget push Panosen.DBSchema.Sqlite\bin\Release\Panosen.DBSchema.Sqlite.*.nupkg -s https://package.savory.cn/v3/index.json --skip-duplicate
-
 move /Y Panosen.DBSchema.Mysql\bin\Release\Panosen.DBSchema.Mysql.*.nupkg D:\LocalSavoryNuget\
 move /Y Panosen.DBSchema.Sqlite\bin\Release\Panosen.DBSchema.Sqlite.*.nupkg D:\LocalSavoryNuget\
 
